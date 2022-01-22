2d6f7a66164795dcbd2f305fdc445eb62f9c99e9
chetan chandel <chetanchandel3@gmail.com>
2022-01-23 00:31:25 +0530

    remove icons

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -1,5 +1,3 @@
 export { default as ArrowDown } from "./ArrowDown";
 export { default as Cross } from "./Cross";
-export { default as Download } from "./Download";
-export { default as Search } from "./Search";
 export { default as User } from "./User";
