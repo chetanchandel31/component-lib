41c5f01ec2e98b06c63dab75e34e2d76456d390a
chetan chandel <chetanchandel3@gmail.com>
2022-01-23 17:12:01 +0530

    haki provider

--- example/index.tsx
+++ example/index.tsx
@@ -1,14 +1,14 @@
 import "react-app-polyfill/ie11";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { Button, Thing } from "../.";
+import { Button, HakiProvider, Thing } from "../.";
 
 const App = () => {
   return (
-    <div>
+    <HakiProvider>
       <Thing />
       <Button color="blue">zz</Button>
-    </div>
+    </HakiProvider>
   );
 };
 
---
+++ src/Providers/HakiProvider.tsx
@@ -0,0 +1,31 @@
+import React, { ReactNode } from "react";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./global.styles";
+
+type HakiProviderProps = {
+  children: ReactNode;
+  theme?: Object; // TODO: change
+};
+
+export const defaultTheme = {
+  primary: {
+    main: "#ffffff",
+  },
+  secondary: {},
+  danger: {},
+  warning: {},
+};
+
+/** applies global styles and ability to customize theme for haki components*/
+export const HakiProvider = (props: HakiProviderProps) => {
+  const { children, theme } = props;
+
+  const finalTheme = { ...defaultTheme, theme };
+
+  return (
+    <ThemeProvider theme={finalTheme}>
+      <GlobalStyles />
+      {children}
+    </ThemeProvider>
+  );
+};
