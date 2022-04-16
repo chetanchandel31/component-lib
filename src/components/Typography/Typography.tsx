de2f14a750421e33affa6afb1dcc6417c2761559
chetan chandel <chetanchandel3@gmail.com>
2022-04-16 14:09:32 +0530

    description for as prop

--- src/components/Typography/Typography.tsx
+++ src/components/Typography/Typography.tsx
@@ -15,6 +15,7 @@ export type HakiTypographyColor = "default" | ColorName | "disabled";
 
 /* props-start */
 type BaseTextProps = {
+  /** The component used for the root node. Either a string to use a HTML element or a component. */
   as?: any;
   children: ReactNode;
   className?: string;
